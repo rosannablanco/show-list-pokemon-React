fd01ce75dc304f02bbdb5052d1a89d5865679b5e
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-04-07 08:56:16 +0200

    Clean and add structure folder and files components and stylesheets

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../stylesheets/App.scss';
+
+class App extends React.Component {
+  /* constructor(props) {
+    super(props);
+  } */
+
+  render() {
+    return <div className="App">Hola</div>;
+  }
+}
+
+export default App;
